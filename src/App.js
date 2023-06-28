48deaa552b81bad3dd2df8ae7759c7cdaffa9165
Nirushana <kavinda.n.t@gmail.com>
2023-06-28 18:25:45 +0100

    Portfolio

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Nav from "./Components/Nav/Nav.js"
+
+function App() {
+  return (
+    <Nav />
+  );
+}
+
+export default App;
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./styles.css"
+
+function Nav() {
+    return (
+        <nav>
+            <a className='logo'>kN<span>.</span></a>
+            <div className='navbar'>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">PORTFOLIO</a></li>
+                    <li><a href="#">CONTACT ME</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
