aa8d962f97f4c149b2a727c336a2f1f812144aec
Nirushana <kavinda.n.t@gmail.com>
2023-06-28 21:23:38 +0100

    Landing Page

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -4,20 +4,26 @@ import styles from "./styles.css"
 const Landing = () => {
     return (
         <div className='container'>
-            <div className='left_container'>
-                <div className='left_top'>
-                    DEVELOPER
+            <div className='topSub_container'>
+                <div className='left_container'>
+                    <div className='left_top'>
+                        DEVELOPER
+                    </div>
+                    <div className='class-name'></div>
+                    <div className='left_bottom'>
+                        UI/UX DESIGNER
+                    </div>
                 </div>
-                <div className='class-name'></div>
-                <div className='left_bottom'>
-                    UI/UX DESIGNER
+                <div className='right_container'>
+                    <div className='right_top'>
+                        I'm Kavinda Nirushana
+                    </div>
+                    <a href='#' className='btn'>CONTACT ME</a>
                 </div>
             </div>
-            <div className='right_container'>
-                <div className='right_top'>
-                    I'm Kavinda Nirushana
-                </div>
-                <a href='#' className='btn'>CONTACT ME</a>
+
+            <div className='bottomSub_container'>
+                Web Application - Mobile Application - Websites
             </div>
         </div>
     )
