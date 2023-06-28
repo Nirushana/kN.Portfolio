1e6d6612c395d0e6f5a1bdce5593f759e9af50e3
Nirushana <kavinda.n.t@gmail.com>
2023-06-28 20:35:02 +0100

    Landing Page

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -5,13 +5,19 @@ const Landing = () => {
     return (
         <div className='container'>
             <div className='left_container'>
-                <div className='top'>
+                <div className='left_top'>
                     DEVELOPER
                 </div>
                 <div className='class-name'></div>
-                <div className='bottom'>
+                <div className='left_bottom'>
                     UI/UX DESIGNER
                 </div>
+            </div>
+            <div className='right_container'>
+                <div className='right_top'>
+                    I'm Kavinda Nirushana
+                </div>
+                <a href='#' className='btn'>CONTACT ME</a>
             </div>
         </div>
     )
