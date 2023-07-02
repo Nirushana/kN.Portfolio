13fdc253177a7f443e2882d83a677042fa13587d
Nirushana <kavinda.n.t@gmail.com>
2023-07-02 19:08:41 +0100

    Navbar Responsive

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import navStyles from "./navStyles.css"
 
-function Nav() {
-    return (
-        <nav>
-            <a className='logo'>kN<span>.</span></a>
-            <div className='navbar'>
-                <ul>
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="#">ABOUT</a></li>
-                    <li><a href="#">PORTFOLIO</a></li>
-                    <li><a href="#">CONTACT ME</a></li>
-                </ul>
-            </div>
-        </nav>
-    )
+class Nav extends Component {
+    state = { clicked: false }
+    handleClick = () => {
+        this.setState({clicked:
+        !this.state.clicked})
+    }
+
+    render() {
+        return (
+            <nav>
+                <a className='logo'>kN<span>.</span></a>
+                <div className='navbar'>
+                    <ul>
+                        <li><a href="#">HOME</a></li>
+                        <li><a href="#">ABOUT</a></li>
+                        <li><a href="#">PORTFOLIO</a></li>
+                        <li><a href="#">CONTACT ME</a></li>
+                    </ul>
+                </div>
+
+                <div id='mobile' onClick={this.handleClick}>
+                    <i id='bar' className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+            </nav>
+        )
+    }
 }
 
 export default Nav
