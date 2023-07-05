14dab7d8f87813bc1b8145acff74252891571361
Nirushana <kavinda.n.t@gmail.com>
2023-07-05 13:40:27 +0100

    Navbar Responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./Components/About/About.js";
 import Landing from "./Components/Landing/Landing.js";
 import Nav from "./Components/Nav/Nav.js"
+import Projects from "./Components/Projects/Projects.js";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Landing />
       <About />
+      <Projects />
     </>
 
   );
