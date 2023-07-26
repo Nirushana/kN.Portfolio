0db77bfad68d6bc82a1664d6a17add3694c77d9d
Nirushana <kavinda.n.t@gmail.com>
2023-07-26 16:06:37 +0100

    Projects Section

--- src/Components/Projects/Works.js
+++ src/Components/Projects/Works.js
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import { projectsData, projectNav } from './data'
 
 function Works() {
   return (
     <div>
-      <h1>Hello Works</h1>
+      <div className='work_filters'>
+        {projectNav.map((item, index) => {
+          return (
+            <span className='work_items' key={index}>{item.name}</span>
+          )
+        })}
+      </div>
+
+      <div className='work_container container grid'>
+        {projectsData.map((item) =>{
+          return <WorksItems />
+        })}
+      </div>
     </div>
   )
 }
--- src/Components/Projects/data.js
+++ src/Components/Projects/data.js
@@ -18,13 +18,42 @@ export const projectsData = [
         id: 3,
         image: work3,
         title: 'Ryman Webiste Landing Page',
-        category: 'ui/ux',
+        category: 'web',
     },
 
     {
         id: 4,
         image: work4,
         title: 'Cash Back App - UI/UX',
-        category: 'ui/ux',
+        category: 'app',
     },
-]
+
+    {
+        id: 5,
+        image: work5,
+        title: 'AI Image & Video enhancer',
+        category: 'ai',
+    },
+];
+
+export const projectNav = [
+    {
+        name: 'all',
+    },
+
+    {
+        name: 'web',
+    },
+
+    {
+        name: 'app',
+    },
+
+    {
+        name: 'ui/ux',
+    },
+
+    {
+        name: 'ai'
+    },
+];
