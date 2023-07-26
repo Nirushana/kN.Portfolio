c44b1a810a49dae5b0e6a36c4b74dd6cf6592d6c
Nirushana <kavinda.n.t@gmail.com>
2023-07-26 15:16:17 +0100

    Projects Section

---
+++ src/Components/Projects/data.js
@@ -0,0 +1,30 @@
+
+export const projectsData = [
+    {
+        id: 1,
+        image: work1,
+        title: 'FixMate - UI/UX Case Study',
+        category: 'ui/ux',
+    },
+
+    {
+        id: 2,
+        image: work2,
+        title: 'EV Charging App - UI/UX',
+        category: 'ui/ux',
+    },
+
+    {
+        id: 3,
+        image: work3,
+        title: 'Ryman Webiste Landing Page',
+        category: 'ui/ux',
+    },
+
+    {
+        id: 4,
+        image: work4,
+        title: 'Cash Back App - UI/UX',
+        category: 'ui/ux',
+    },
+]
