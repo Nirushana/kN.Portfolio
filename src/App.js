76cdbf37e6715cf4a9d702129cd14ca2c0464d25
Nirushana <kavinda.n.t@gmail.com>
2023-09-25 20:26:54 +0100

    Projects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import About from "./Components/About/About.js";
 import Landing from "./Components/Landing/Landing.js";
 import Nav from "./Components/Nav/Nav.js"
-import Projects from "./Components/Projects/Projects.js";
+// import Projects from "./Components/Projects/Projects.js";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Nav />
       <Landing />
       <About />
-      <Projects />
+      {/* <Projects /> */}
     </>
 
   );
